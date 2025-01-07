7c850c52525a7b2285942ced4e0492192c29c11c
Aryan10 <aryan.shivam.10@gmail.com>
2025-01-07 11:24:40 +0530

    chore: Add User model and setup MongoRepository

--- main/src/main/java/com/vandus/main/repository/UserRepository.java
+++ main/src/main/java/com/vandus/main/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package com.vandus.main.repository;
 
-public class UserRepository {
-    
+import com.vandus.main.model.User;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
