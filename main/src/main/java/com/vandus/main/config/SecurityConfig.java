5957c11acc809fed160c414088351f8d697d8453
Aryan10 <aryan.shivam.10@gmail.com>
2025-01-09 17:11:26 +0530

    refactor: Add property csrf.enabled and public api route /auth/_csrf

--- main/src/main/java/com/vandus/main/config/SecurityConfig.java
+++ main/src/main/java/com/vandus/main/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import com.vandus.main.security.JWTAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -13,7 +13,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -21,10 +20,15 @@ public class SecurityConfig {
     @Autowired
     private JWTAuthFilter jwtAuthFilter;
 
+    @Value("${vandus.csrf.enabled}")
+    private boolean csrfEnabled;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        if (!csrfEnabled)
+            http.csrf(csrfCustomizer -> csrfCustomizer.disable());
+        
         return http
-            .csrf(csrfCustomizer -> csrfCustomizer.disable())
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
--- main/src/main/java/com/vandus/main/controller/SecurityController.java
+++ main/src/main/java/com/vandus/main/controller/SecurityController.java
@@ -1,15 +1,28 @@
 package com.vandus.main.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("${vandus.api.public}/auth")
 public class SecurityController {
 
-    @GetMapping("/csrf-token")
-    public CsrfToken getCsrfToken(HttpServletRequest request) {
-        return (CsrfToken) request.getAttribute("_csrf");
+    @Value("${vandus.csrf.enabled}")
+    private boolean csrfEnabled;
+
+    @GetMapping("/_csrf")
+    public ResponseEntity<?> getCsrfToken(HttpServletRequest request) {
+        if (!csrfEnabled)
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("CSRF protection is disabled");
+        
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+        return ResponseEntity.ok(csrfToken);
     }
 }
