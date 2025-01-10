2d45f0373884d0f4535de011e4cacabd241c4ed0
Uditansh Srivastava <krisnarakesh@gmail.com>
2025-01-11 01:03:41 +0530

    refactor: Add GlobalExceptionHandler ErrorMessageHandler util Exception InvalidEmailPasswordException UnableToSendOTPException UserAlreadyExistsException

---
+++ main/src/main/java/com/vandus/main/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.vandus.main.dto;
+
+public class ErrorResponse {
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    private String error;
+
+}
