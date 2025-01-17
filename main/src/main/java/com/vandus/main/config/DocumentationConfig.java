8e8f1c87c194edc19d5193f6dc2b0c217d4f190e
Aryan10 <aryan.shivam.10@gmail.com>
2025-01-18 02:01:01 +0530

    feat: Add Open API Documentation Config

---
+++ main/src/main/java/com/vandus/main/config/DocumentationConfig.java
@@ -0,0 +1,19 @@
+package com.vandus.main.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DocumentationConfig {
+
+    @Bean
+    public OpenAPI apiDocumentation() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("TransformoDocs")
+                .description("API for TransformoDocs")
+                .version("1.0.0"));
+    }
+}
