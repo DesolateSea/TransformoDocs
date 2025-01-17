db3bc968682ce00cafd668a813e2081aeb9f5d0e
Aryan10 <aryan.shivam.10@gmail.com>
2025-01-17 22:28:12 +0530

    feat: Add route /api/private/user/ to get user details from authentication token

--- main/src/main/java/com/vandus/main/controller/UserController.java
+++ main/src/main/java/com/vandus/main/controller/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.vandus.main.model.User;
 import com.vandus.main.service.UserService;
+import com.vandus.main.util.JwtUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("${vandus.api.private}/user")
@@ -15,6 +18,16 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/")
+    public User getUser(HttpServletRequest request) {
+        String token = jwtUtil.extractTokenFromRequest(request);
+        String email = jwtUtil.getEmail(token);
+        return userService.getUserByEmail(email);
+    }
     
     @GetMapping("/{id}")
     public User getUserById(@PathVariable("id") String id) {
