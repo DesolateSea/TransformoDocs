28f7178d09937455119868e889d7c444a1b12a27
Aryan10 <aryan.shivam.10@gmail.com>
2025-01-21 23:44:07 +0530

    feat: Add multiple documentation routes e.g. /api-docs/auth

--- main/src/main/java/com/vandus/main/config/DocumentationConfig.java
+++ main/src/main/java/com/vandus/main/config/DocumentationConfig.java
@@ -2,11 +2,23 @@ package com.vandus.main.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DocumentationConfig {
+
+    @Value("${vandus.api.public}")
+    private String apiPublic;
+
+    @Value("${vandus.api.private}")
+    private String apiPrivate;
+
+    @Value("${vandus.api.auth}")
+    private String apiAuth;
 
     @Bean
     public OpenAPI apiDocumentation() {
@@ -15,5 +27,29 @@ public class DocumentationConfig {
                 .title("TransformoDocs")
                 .description("API for TransformoDocs")
                 .version("1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi authDocumentation() {
+        return GroupedOpenApi.builder()
+            .group("auth")
+            .pathsToMatch(apiAuth + "/**")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi publicDocumentation() {
+        return GroupedOpenApi.builder()
+            .group("public")
+            .pathsToMatch(apiPublic + "/**")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi privateDocumentation() {
+        return GroupedOpenApi.builder()
+            .group("private")
+            .pathsToMatch(apiPrivate + "/**")
+            .build();
     }
 }
