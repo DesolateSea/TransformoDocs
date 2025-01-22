53316776f16284886627f6b8e6255a199e885f8b
Aryan10 <aryan.shivam.10@gmail.com>
2025-01-23 00:37:49 +0530

    fix: Fix bug with cors cnfigutation requiring exact url by using allowedOriginPatterns

--- main/src/main/java/com/vandus/main/config/WebConfig.java
+++ main/src/main/java/com/vandus/main/config/WebConfig.java
@@ -15,8 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(clientUrl)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOriginPatterns(clientUrl)
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
