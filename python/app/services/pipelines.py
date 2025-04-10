857bffc49973bbdcf967f74af7b787fe57be8ebb
Uditansh Srivastava <krisnarakesh@gmail.com>
2025-04-10 16:45:55 +0530

    fix: update NER pipeline aggregation strategy to max for improved entity merging

--- python/app/services/pipelines.py
+++ python/app/services/pipelines.py
@@ -1,7 +1,14 @@
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer
 
-# Named Entity Recognition (NER) pipeline
-ner_pipeline = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english")
+tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-large-cased-finetuned-conll03-english")
+ner_pipeline = pipeline(
+    "ner",
+    model="dbmdz/bert-large-cased-finetuned-conll03-english",
+    tokenizer=tokenizer,
+    aggregation_strategy="max"
+)
+
+
 
 # Sentiment Analysis pipeline
 sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
